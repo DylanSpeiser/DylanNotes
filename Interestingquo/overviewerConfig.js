ab25f5a22a26f9a531bd497f354d6c03f2adbda8
DylanSpeiser <bluedisney16@gmail.com>
2020-12-09 13:12:36 -0500

    new map 12/9

--- Interestingquo/overviewerConfig.js
+++ Interestingquo/overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1607416292",
+        "cacheTag": "1607537431",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1607415474,
+            "last_rendertime": 1607493902,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
